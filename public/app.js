a1d5f7286ed9844011004e1ef01f6eb8f918fa1c
anangyoga <aharwinto@gmail.com>
2022-04-16 10:42:11 +0700

    enums

--- public/app.js
+++ public/app.js
@@ -1,35 +1,25 @@
 "use strict";
-// Generics
-// It allows you to create reusable block of code that can be used with different types
-// T will chapture whatever item we pass to the function
-const addUID = (obj) => {
-    let uid = Math.floor(Math.random() * 100);
-    return Object.assign(Object.assign({}, obj), { uid });
+// Enums
+// a way to specify descriptive constant and associate each one with numeric value behind the scene (on the console)
+// Enums is zero-based just like index
+var resourceType;
+(function (resourceType) {
+    resourceType[resourceType["BOOK"] = 0] = "BOOK";
+    resourceType[resourceType["AUTHOR"] = 1] = "AUTHOR";
+    resourceType[resourceType["FILM"] = 2] = "FILM";
+    resourceType[resourceType["DIRECTOR"] = 3] = "DIRECTOR";
+    resourceType[resourceType["PERSON"] = 4] = "PERSON";
+})(resourceType || (resourceType = {}));
+const docOne = {
+    uid: 23,
+    //this one, we don't need to remember the index/number of certain constant
+    resourceType: resourceType.AUTHOR,
+    data: { name: "Jonas Jonasson" },
 };
-let docOne = addUID({ name: "Tim", age: 20 });
-console.log(docOne.age);
-// T extends object, whatever we pass must be an object
-const addUIDnum2 = (obj) => {
-    let uid = Math.floor(Math.random() * 100);
-    return Object.assign(Object.assign({}, obj), { uid });
+const docTwo = {
+    uid: 19,
+    // and this one, we still use number/index to associate constant
+    resourceType: 4,
+    data: { name: "Gillian" },
 };
-let docTwo = addUIDnum2({ name: "Tim", age: 20 });
-console.log(docTwo);
-const docThree = {
-    uid: 1,
-    resourceName: "data",
-    // this part should be a string because the T is now string as we declared
-    data: "the data is lorem ipsum",
-};
-const docFour = {
-    uid: 1,
-    resourceName: "person",
-    // this part should be object because the T is now object as we declared
-    data: { name: "TIm" },
-};
-const docFive = {
-    uid: 1,
-    resourceName: "shoppinglist",
-    // this part should be object because the T is now object as we declared
-    data: ["pear", "strawberry", "pineapple"],
-};
+console.log(docOne, docTwo);
--- src/app.ts
+++ src/app.ts
@@ -1,48 +1,33 @@
-// Generics
-// It allows you to create reusable block of code that can be used with different types
+// Enums
+// a way to specify descriptive constant and associate each one with numeric value behind the scene (on the console)
+// Enums is zero-based just like index
 
-// T will chapture whatever item we pass to the function
-const addUID = <T>(obj: T) => {
-  let uid = Math.floor(Math.random() * 100);
-  return { ...obj, uid };
-};
+enum resourceType {
+  BOOK,
+  AUTHOR,
+  FILM,
+  DIRECTOR,
+  PERSON,
+}
 
-let docOne = addUID({ name: "Tim", age: 20 });
-console.log(docOne.age);
-
-// T extends object, whatever we pass must be an object
-const addUIDnum2 = <T extends object>(obj: T) => {
-  let uid = Math.floor(Math.random() * 100);
-  return { ...obj, uid };
-};
-
-let docTwo = addUIDnum2({ name: "Tim", age: 20 });
-console.log(docTwo);
-
-// Generics with Interfaces
-interface Resources<T> {
+interface Resource<T> {
   uid: number;
-  resourceName: string;
+  resourceType: resourceType;
   data: T;
 }
 
-const docThree: Resources<string> = {
-  uid: 1,
-  resourceName: "data",
-  // this part should be a string because the T is now string as we declared
-  data: "the data is lorem ipsum",
+const docOne: Resource<object> = {
+  uid: 23,
+  //this one, we don't need to remember the index/number of certain constant
+  resourceType: resourceType.AUTHOR,
+  data: { name: "Jonas Jonasson" },
 };
 
-const docFour: Resources<object> = {
-  uid: 1,
-  resourceName: "person",
-  // this part should be object because the T is now object as we declared
-  data: { name: "TIm" },
+const docTwo: Resource<object> = {
+  uid: 19,
+  // and this one, we still use number/index to associate constant
+  resourceType: 4,
+  data: { name: "Gillian" },
 };
 
-const docFive: Resources<string[]> = {
-  uid: 1,
-  resourceName: "shoppinglist",
-  // this part should be object because the T is now object as we declared
-  data: ["pear", "strawberry", "pineapple"],
-};
+console.log(docOne, docTwo);
